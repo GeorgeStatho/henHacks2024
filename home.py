bc2a7eaf8798a1f476e02a060b27425285235157
GeorgeStatho <125417269+GeorgeStatho@users.noreply.github.com>
2024-03-02 15:21:33 -0500

    start of map

--- home.py
+++ home.py
@@ -1,5 +1,7 @@
 from flask import Flask
+import requests
 app = Flask(__name__)
+url="https://us1.locationiq.com/v1/reverse?key=pk.5bc12968439e6e5392783b5b71ad364a&lat=LATITUDE&lon=LONGITUDE&format=json"
 
 @app.route("/")
 def hello_world():
