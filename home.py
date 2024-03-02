12d38ec1422596ba2523a44b43c5c5bd5ed27936
GeorgeStatho <125417269+GeorgeStatho@users.noreply.github.com>
2024-03-02 16:46:12 -0500

    Displays map

--- home.py
+++ home.py
@@ -1,16 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template_string
+import folium
 import requests
 app = Flask(__name__)
-url = "https://us1.locationiq.com/v1/reverse"
-data = {
-        'key': 'pk.5bc12968439e6e5392783b5b71ad364a',
-        'lat': '-37.870662',
-        'lon': '144.9803321',
-        'format': 'json'
-    }
-response = requests.get(url, params=data)
-print(response.text)
-@app.route("/")
-def map_data():
-    return response.text
 
+def add_markers():
+    return 0
+
+
+@app.route("/")
+def display_map_data():
+    m=folium.Map(location=(39.683723, -75.749657))
+    m.get_root().width = "800px"
+    m.get_root().height = "600px"
+    iframe = m.get_root()._repr_html_()
+
+    return render_template_string(
+        """
+            <!DOCTYPE html>
+            <html>
+                <head></head>
+                <body>
+                    <h1>People near you with same intrests.</h1>
+                    {{ iframe|safe }}
+                </body>
+            </html>
+        """,
+        iframe=iframe,
+    )
