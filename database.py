62b187b5dd0563ba53cd49510e43fa0fc60d3bd0
Alexander <alexcham@udel.edu>
2024-03-02 22:01:30 -0500

    Yea

--- database.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class Person:
-    Name : str
-    Address : str
-    Interest : str
-
-users = [Person("Example name", "255 Academy St", "Board Games")]
