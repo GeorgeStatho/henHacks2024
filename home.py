26ec58ef39bad7fe6872cb04a8d0aeed636a1ff5
GeorgeStatho <125417269+GeorgeStatho@users.noreply.github.com>
2024-03-02 21:59:56 -0500

    added fs module

--- home.py
+++ home.py
@@ -2,14 +2,15 @@ from flask import Flask, render_template_string
 import folium
 import requests
 from database import users
+from database import Person
 import math
 
 app = Flask(__name__)
 
-def convert_Address_To_Coord(person):
+def convert_Address_To_Coord(person:Person):
     #Assuming Adresses will be with Street name and number, town, state, zipcode, in that order
     #Note: Street Number and name assumed to have no space inbetween them.
-    address=person.address.split()
+    address=person.Address.split()
     url = "https://us1.locationiq.com/v1/search/structured?key=pk.5bc12968439e6e5392783b5b71ad364a"
     for i in range(len(address)-1):
         if(i<len(address)-1):
@@ -28,16 +29,17 @@ def get_distance(lat1, lat2, lon1, lon2):
     distmile = distkm / 1.609
     return distmile
 
-def place_marker(target,people,max_distance,map):
+def place_marker(target:Person,people:list,max_distance:int,map):
     target_lat,target_lon=convert_Address_To_Coord(target)
     for person in people:
         person_lat,person_lon=convert_Address_To_Coord(person)
-        if(get_distance(target_lat,target_lon,person_lat,person_lon)<max_distance):
+        if(get_distance(target_lat,target_lon,person_lat,person_lon)<max_distance and target.Interest==person.Interest):
             folium.Marker(location=[person_lat,person_lon],name=person.name).add_to(map)
 
 @app.route("/")
 def display_map_data():
     m=folium.Map(location=(39.683723, -75.749657))
+    place_marker(users[-1],users,5,m)
     m.get_root().width = "800px"
     m.get_root().height = "600px"
     iframe = m.get_root()._repr_html_()
